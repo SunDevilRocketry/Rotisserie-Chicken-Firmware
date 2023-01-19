d384937329eb2586e12568d8fb1b67cd9a7f4b6a
Colton Acosta <cacost12@asu.edu>
2023-01-19 13:26:48 -0700

    inital code commit

--- Rotisserie-Chicken/Rotisserie-Chicken.ino
+++ Rotisserie-Chicken/Rotisserie-Chicken.ino
@@ -5,8 +5,10 @@ const int motorBrakePin = 9; // for motor A
 const int CW  = HIGH;
 const int CCW = LOW;
 
-int motorSpeed = 255;
+int motorSpeed = 200;
 int initMotorSpeed = 255;
+
+long int current_time, start_time;
 
 void setup() {
   // motor A pin assignment
@@ -19,18 +21,22 @@ void setup() {
   delay(100);
  
   
-  Serial.begin(9600);//  seial monitor initialized 
+  Serial.begin(9600);//  seial monitor initialized
+  start_time = millis();
 
 }
 
 void loop() {
 
-  
-  digitalWrite(motorDirectionPin, CW);// set direction
-  analogWrite(motorSpeedPin, motorSpeed);// set speed at maximum
-  //delay(100);
-  //analogWrite(motorSpeedPin, motorSpeed);// set speed at maximum
-  //analogWrite(motorSpeedPin, 0);// set speed at maximum
-  //delay(10000);
- 
+  current_time = millis() - start_time;
+  if(current_time <= 7200000 )
+    {
+    digitalWrite(motorDirectionPin, CW);// set direction
+    analogWrite(motorSpeedPin, motorSpeed);// set speed at maximum
+    delay(500);
+    analogWrite(motorSpeedPin, motorSpeed);// set speed at maximum
+    analogWrite(motorSpeedPin, 0);// set speed at maximum
+    delay(10000);
+    }
+    
 }// loop end
